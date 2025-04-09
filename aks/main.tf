a9fc666ceb2cbf45d29641793998c0168396340c
Marcelo Paglione <104773898+MarceloPaglione2411@users.noreply.github.com>
2025-04-09 18:55:26 -0300

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -1,1 +1,31 @@
+resource "azurerm_kubernetes_cluster" "my-aks" {
+  name                = var.aks_name
+  location            = var.rg_location
+  resource_group_name = var.rg_name
+  dns_prefix = var.dns_aks
+  
+  default_node_pool {
+    name       = var.name_nodepool
+    node_count = 1
+    vm_size    = var.vm_size
+  }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+#output "client_certificate" {
+#  value     = azurerm_kubernetes_cluster.example.kube_config[0].client_certificate
+#  sensitive = true
+#}
+
+#output "kube_config" {
+#  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+#  sensitive = true
+#}
